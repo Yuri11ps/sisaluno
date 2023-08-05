37dc6307c15190aeffa1e68c463d0f8c9daa1323
Yuri Paulo <130406601+Yuri11ps@users.noreply.github.com>
2023-08-05 10:13:04 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SisAluno Yuri Paulo</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="caixa_index">
+        <div class="menu">
+            <h1>SisAluno Yuri Paulo</h1>
+            <a href="aluno/index.php">Gerenciar os Alunos</a>
+            <br>
+            <br>
+            <a href="professor/index.php">Gerenciar os Professores</a>
+            <br>
+            <br>
+            <a href="disciplina/index.php">Gerenciar as Disciplinas</a>
+        </div>
+    </div>
+</body>
+</html>
